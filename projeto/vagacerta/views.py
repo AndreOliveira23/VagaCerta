e02cee9eb3c47581d296aafa748e71eadf49e232
Gustavo Ventorim Silva <gustavo.ventorim@usp.br>
2024-11-20 12:53:42 -0300

    arrumando arquivos

--- projeto/vagacerta/views.py
+++ projeto/vagacerta/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login_page(request):
+    return render(request, 'vagacerta/login.html')
+
+def index_page(request):
+    return render(request, 'vagacerta/index.html')
+
+def searchBar(request):
+    return render(request, 'vagacerta/searchBar.html')
