b4697f57f9cb75d6a84ca2b35c8dafad8130ef44
Gabriel Ventorim <gabriel.ventorims@gmail.com>
2024-11-20 16:58:12 -0300

    tela de pagamento

--- projeto/vagacerta/static/js/nav.js
+++ projeto/vagacerta/static/js/nav.js
@@ -6,3 +6,11 @@ function abrirSidebar(){
 function fecharSidebar(){
     document.querySelector('#dialog').close();
 }
+
+function mostrarPagamento(tipo) {
+    const opcoes = document.querySelectorAll('.pagamento-opcao');
+    opcoes.forEach(opcao => opcao.style.display = 'none');
+
+    const selecionado = document.getElementById(tipo);
+    selecionado.style.display = 'block';
+  }
