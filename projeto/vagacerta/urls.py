b4a9f640e40968a65f6799c7b378f91ea623f9ab
Gustavo Ventorim Silva <gustavo.ventorim@usp.br>
2024-11-23 16:04:18 -0300

    urls arrumadas

--- projeto/vagacerta/urls.py
+++ projeto/vagacerta/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('login/', views.login_page, name='login'),
     path('index/', views.index_page, name='index'),
-    path('search/', views.searchBar, name='search')
+    path('confirmation/', views.confirmation_page, name='confirmation'),
+    path('analise/', views.analiseEstacionamento_page, name='analise'),
+    path('form/', views.formEstacionamento_page, name='form'),
+    path('intrapage/', views.intra_page, name='intra'),
+    path('pagamento/', views.pagamento_page, name='pagamento'),
+    path('recibo/', views.recibo_page, name='recibo'),  
 ]
--- projeto/vagacerta/views.py
+++ projeto/vagacerta/views.py
@@ -9,3 +9,21 @@ def index_page(request):
 
 def searchBar(request):
     return render(request, 'vagacerta/searchBar.html')
+
+def confirmation_page(request):
+    return render(request, 'vagacerta/confirmation.html')
+
+def formEstacionamento_page(request):
+    return render(request, 'vagacerta/form-estacionamento.html')
+
+def analiseEstacionamento_page(request):
+    return render(request, 'vagacerta/analise-estacionamento.html')
+
+def intra_page(request):
+    return render(request, 'vagacerta/intrapage.html')
+
+def recibo_page(request):
+    return render(request, 'vagacerta/recibo.html')
+
+def pagamento_page(request):
+    return render(request, 'vagacerta/pagamento.html')
